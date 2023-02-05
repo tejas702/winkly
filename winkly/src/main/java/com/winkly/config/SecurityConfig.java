c1cffcfc62be39020fdb9db249a33b9d624c4bfa
rudransh-24 <rudransh.tripathi@groww.in>
2023-02-05 20:53:03 +0530

    removing cors issue

--- winkly/src/main/java/com/winkly/config/SecurityConfig.java
+++ winkly/src/main/java/com/winkly/config/SecurityConfig.java
@@ -14,11 +14,13 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @Configuration
 @EnableMethodSecurity(
         securedEnabled = true,
         jsr250Enabled = true)
+@CrossOrigin(allowedHeaders = {"*", "Content-Type", "token", "authorization"}, origins = "*")
 public class SecurityConfig {
 
     @Autowired
@@ -54,7 +56,7 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.cors().and().csrf().disable()
+        http.csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
                 .authorizeRequests().antMatchers("/winkly**", "/swagger-ui.html**",
