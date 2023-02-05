14bb1a1073baba327b2c0e4d5d7b7a8b994d111b
rudransh-24 <rudransh.tripathi@groww.in>
2023-02-06 01:24:41 +0530

    sending token at sign up

--- winkly/src/main/java/com/winkly/controller/RegistrationController.java
+++ winkly/src/main/java/com/winkly/controller/RegistrationController.java
@@ -1,22 +1,27 @@
 package com.winkly.controller;
 
 import com.winkly.config.JwtUtils;
-import com.winkly.dto.MessageInfoDto;
-import com.winkly.dto.PasswordDto;
-import com.winkly.dto.ResetPasswordDto;
-import com.winkly.dto.UserRegisterRequestDto;
+import com.winkly.dto.*;
+import com.winkly.entity.RefreshToken;
 import com.winkly.entity.UserEntity;
 import com.winkly.repository.UserRepository;
+import com.winkly.service.impl.RefreshTokenServiceImpl;
+import com.winkly.service.impl.UserDetailsImpl;
 import com.winkly.service.impl.UserDetailsServiceImpl;
 import com.winkly.utils.GenerateResetToken;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailSender;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -29,6 +34,7 @@ import java.util.Optional;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 @RequestMapping(value = "/winkly")
 @Api(tags = "Registration Controller")
 public class RegistrationController {
@@ -52,6 +58,10 @@ public class RegistrationController {
     @Autowired
     private UserDetailsServiceImpl userDetailsService;
 
+
+    @Autowired
+    private RefreshTokenServiceImpl refreshTokenService;
+
     @PostMapping("/register_user")
     @ApiOperation("Register User")
     public ResponseEntity registerUser(@Valid @RequestBody UserRegisterRequestDto userRegisterRequestDto) {
@@ -65,7 +75,20 @@ public class RegistrationController {
 
         userRepository.save(user);
 
-        return ResponseEntity.ok(new MessageInfoDto("User registered successfully!"));
+        Authentication authentication = authenticationManager
+                .authenticate(new UsernamePasswordAuthenticationToken(userRegisterRequestDto.getEmail(),
+                        userRegisterRequestDto.getPassword()));
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
+        UserDetailsImpl userDetails = (UserDetailsImpl) authentication.getPrincipal();
+
+        ResponseCookie jwtCookie = jwtUtils.generateJwtCookie(userDetails);
+
+        RefreshToken refreshToken = refreshTokenService.createRefreshToken(userDetails.getEmail());
+
+        return ResponseEntity.ok().body(new MultipleMessageDto(new JwtResponseDto(jwtCookie.getValue(), refreshToken.getToken(), userDetails.getId(),
+                userDetails.getUsername(), userDetails.getEmail()), "User Registered Successfully"));
     }
 
     @PostMapping("/forgot_password")
---
+++ winkly/src/main/java/com/winkly/dto/MultipleMessageDto.java
@@ -0,0 +1,17 @@
+package com.winkly.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class MultipleMessageDto {
+    private JwtResponseDto jwtResponseDto;
+    private String message;
+}
